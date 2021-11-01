3b34b6cc01d8fd733c4e71a3ceae4647e77957bd
mackristofer <mackristofer@gmail.com>
2021-10-31 21:45:00 -0300

    Commit

---
+++ bds05-main-master/src/main/java/com/devsuperior/movieflix/dto/ReviewDTO.java
@@ -0,0 +1,53 @@
+package com.devsuperior.movieflix.dto;
+
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import java.io.Serializable;
+
+public class ReviewDTO implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private String text;
+  private Movie movie;
+
+  public ReviewDTO() {
+  }
+
+  public ReviewDTO(final Long id, final String text, final Movie movie) {
+    this.id = id;
+    this.text = text;
+    this.movie = movie;
+  }
+
+  public ReviewDTO(Review entity) {
+    id = entity.getId();
+    text = entity.getText();
+    movie = entity.getMovie();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(final Long id) {
+    this.id = id;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(final String text) {
+    this.text = text;
+  }
+
+  public Movie getMovie() {
+    return movie;
+  }
+
+  public void setMovie(final Movie movie) {
+    this.movie = movie;
+  }
+}
